801e413751a8bdbf6e16d99ace772e2cc46f3a26
MoSalah07 <notway10@yahoo.com>
2024-02-24 23:31:09 +0200

    change auth

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/pusher/auth";
+import { authOptions } from "@/auth";
 
 export default async function getSession() {
   return await getServerSession(authOptions);
